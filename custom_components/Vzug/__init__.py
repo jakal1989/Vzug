4b5e0f855e00b6cd504653a8f00fdf8772b84f62
jakal1989 <stefano.cugis@gmail.com>
2024-09-11 19:44:08 +0200

    Update __init__.py

--- custom_components/Vzug/__init__.py
+++ custom_components/Vzug/__init__.py
@@ -1,14 +1,23 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
+"""The V-ZUG Integration."""
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up V-ZUG Integration from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+from homeassistant import config_entries, core
+from homeassistant.const import CONF_IP_ADDRESS
+
+DOMAIN = "vzug"
+
+async def async_setup(hass: core.HomeAssistant, config: dict):
+    """Set up the V-ZUG integration."""
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry):
+    """Set up V-ZUG from a config entry."""
+    # Hier kannst du weitere Initialisierungen vornehmen
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry):
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if DOMAIN in hass.data:
+        hass.data[DOMAIN].pop(entry.entry_id)
     return True
