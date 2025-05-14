18c5471109a81dc180cfe07fd9b8517022b9877f
jakal1989 <stefano.cugis@gmail.com>
2025-05-14 12:08:35 +0200

    Update sensor.py

--- custom_components/Vzug/sensor.py
+++ custom_components/Vzug/sensor.py
@@ -1,65 +1,54 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import CONF_NAME
+from homeassistant.helpers.entity import EntityCategory
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
-import requests
+import aiohttp
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up the V-ZUG sensors based on a config entry."""
-    dishwasher_ip = entry.data["dishwasher_ip"]
-    oven_ip = entry.data["oven_ip"]
+    """Set up V-ZUG sensors from config entry."""
+    device_type = entry.data["device_type"]
+    ip_address = entry.data["ip_address"]
 
-    sensors = [
-        VZugSensor(
-            name="V-ZUG Dishwasher",
-            resource=f"http://{dishwasher_ip}/ai?command=getDeviceStatus",
-            icon="mdi:dishwasher",
-            attributes=["DeviceName", "Serial", "Inactive", "Program", "Status", "ProgramEnd", "End", "EndType", "deviceUuid"]
-        ),
-        VZugSensor(
-            name="V-ZUG Oven",
-            resource=f"http://{oven_ip}/ai?command=getDeviceStatus",
-            icon="mdi:stove",
-            attributes=["DeviceName", "Serial", "Inactive", "Program", "Status", "ProgramEnd", "End", "EndType", "deviceUuid"]
-        )
-    ]
-    async_add_entities(sensors, True)
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"http://{ip_address}/ai?command=getDeviceStatus") as response:
+                data = await response.json()
+    except Exception as e:
+        _LOGGER.error("Fehler beim Abrufen der Gerätedaten: %s", e)
+        return
+
+    entities = []
+    for key, value in data.items():
+        entities.append(VZugSensor(key, value, device_type, ip_address))
+
+    async_add_entities(entities, True)
 
 class VZugSensor(SensorEntity):
-    """Representation of a V-ZUG sensor."""
+    """Ein Sensor pro JSON-Key."""
 
-    def __init__(self, name, resource, icon, attributes):
-        self._name = name
-        self._resource = resource
-        self._icon = icon
-        self._attributes = attributes
-        self._state = None
-        self._attr_extra_state_attributes = {}
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def icon(self):
-        return self._icon
+    def __init__(self, key, value, device_type, ip):
+        self._attr_name = f"{device_type} - {key}"
+        self._attr_unique_id = f"{device_type.lower()}_{ip}_{key.lower()}"
+        self._state = value
+        self._key = key
+        self._ip = ip
+        self._device_type = device_type
+        self._attr_icon = "mdi:checkbox-marked-circle-outline"  # oder besser je nach key
 
     @property
     def state(self):
         return self._state
 
-    @property
-    def extra_state_attributes(self):
-        return self._attr_extra_state_attributes
-
-    def update(self):
-        """Fetch new state data for the sensor."""
+    async def async_update(self):
+        """Hole aktuelle Daten vom Gerät."""
         try:
-            response = requests.get(self._resource)
-            data = response.json()
-            self._state = data.get("result")
-            for attr in self._attributes:
-                self._attr_extra_state_attributes[attr] = data.get(attr)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"http://{self._ip}/ai?command=getDeviceStatus") as response:
+                    data = await response.json()
+                    self._state = data.get(self._key)
         except Exception as e:
+            _LOGGER.error("Update fehlgeschlagen für %s: %s", self._attr_name, e)
             self._state = None
-            self._attr_extra_state_attributes = {}
-            _LOGGER.error(f"Error fetching data from {self._resource}: {e}")
